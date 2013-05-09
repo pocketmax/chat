40c731b15c740bca1b4029cde77141228a146470
Max <maxvharper@yahoo.com>
2013-05-09 02:35:25 -0700

    cleaned out unused code in server script

--- server.js
+++ server.js
@@ -3,24 +3,10 @@
  cmd: sudo node server.js
  this file runs on port 81
  */
-var app = require('http').createServer(handler);
+var app = require('http').createServer();
 var io = require('socket.io').listen(app);
-var fs = require('fs');
 
 app.listen(81);
-
-function handler(req, res) {
-    fs.readFile(__dirname + '/index.html',
-            function(err, data) {
-                if (err) {
-                    res.writeHead(500);
-                    return res.end('Error loading index.html');
-                }
-
-                res.writeHead(200);
-                res.end(data);
-            });
-}
 
 io.sockets.on('connection', function(socket) {
     socket.on('news', function(msg) {
